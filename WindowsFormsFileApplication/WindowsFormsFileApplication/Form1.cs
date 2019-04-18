c4a3b0fc44ff0ad457ac2bd3aa8daf75887ded06
Rakesh Neginhal <10506981@mydbs.ie>
2019-04-18 18:35:52 +0100

    Chnages for calculation and sorting

--- WindowsFormsFileApplication/WindowsFormsFileApplication/Form1.cs
+++ WindowsFormsFileApplication/WindowsFormsFileApplication/Form1.cs
@@ -11,6 +11,7 @@ using System.Text.RegularExpressions;
 using System.Configuration;
 
 using System.Windows.Forms;
+using WindowsFormsFileApplication.Utils;
 
 namespace WindowsFormsFileApplication
 {
@@ -20,8 +21,10 @@ namespace WindowsFormsFileApplication
         static bool destinationfile = false;
         string rootpath = string.Empty;
         string destPath = string.Empty;
-        Dictionary<string, string> dictionaryfile = new Dictionary<string, string>();
+        string calcRootPath = string.Empty;
 
+        
+        FilesUtil fileutils = new FilesUtil();
         public Form1()
         {
             InitializeComponent();
@@ -31,6 +34,7 @@ namespace WindowsFormsFileApplication
             button3.Visible = false;
             button4.Visible = false;
             btnSubmit.Visible = false;
+            button6.Visible = false;
 
         }
 
@@ -73,7 +77,7 @@ namespace WindowsFormsFileApplication
                  MessageBox.Show("Select destination directory");
                  return;
              }
-            sortFiles( rootpath, destPath);
+            fileutils.sortFiles( rootpath, destPath);
             comboBox1.Visible = true;
             button4.Visible = true;
             FillCombobox();
@@ -94,179 +98,11 @@ namespace WindowsFormsFileApplication
             comboBox1.SelectedIndex = 0;
 
         }
-        public void sortFiles(string rootpath,string destPath)
-        {
+       
 
-            //Read all text files from a given directory '
-            string[] files = Directory.GetFiles(rootpath, "*.txt");
+        
 
-            for (int i = 0; i < files.Length; i++)
-            {
-
-                //Get the file name from the path
-                string orgFileName = Path.GetFileName(files[i]);
-                Console.WriteLine("Original text file name is::" + orgFileName);
-
-                //create new file 
-                string newfile="sorted" + orgFileName;
-                string sortedFileName = destPath + "\\"+newfile;
-                Console.WriteLine("New sorted file name is::" + sortedFileName);
-
-                // To Read the original file 
-                StreamReader sr = new StreamReader(files[i], Encoding.UTF8);
-
-
-                //Continue to read until you reach end of file
-                string[] wordsSplit = sr.ReadToEnd().Split(' ');
-
-                //Remove Special charters from a string
-                List<string> list = new List<string>();
-
-                Console.WriteLine("Checking for special characters..");
-                string[] dotwords = null;
-                foreach (var singleString in wordsSplit)
-                {
-                    Regex reg = new Regex("[*'\",_&#^@$()€°?!%]");
-                    string singleword = reg.Replace(singleString, string.Empty);
-
-                    if (singleword.Any(c => char.IsDigit(c)))
-                    {
-                        Regex reg1 = new Regex("[*a-zA-Z]");
-                        singleword = reg1.Replace(singleword, string.Empty);
-
-                    }
-                    else if (singleword.Contains(".") || singleword.Contains(","))
-                    {
-                        dotwords = singleword.Split('.');
-                        Console.WriteLine("removing full stop" + singleword);
-
-                    }
-
-                    Console.WriteLine("new words:" + singleword);
-                    list.Add(singleword);
-
-                }
-                string[] listwords = list.ToArray();
-
-                var words = listwords.OrderBy(s => s, new MyComparer());       
-                
-                //count the number of occurences 
-                var dirWordCount = new Dictionary<string, int>();
-
-                foreach (var word in words)
-                {
-                    if (dirWordCount.ContainsKey(word))
-                    {
-                        dirWordCount[word] = dirWordCount[word] + 1;
-                    }
-                    else
-                    {
-                        dirWordCount.Add(word, 1);
-                    }
-                }
-
-
-                //now write the contents into sorted file
-                for (int index = 0; index < dirWordCount.Count; index++)
-                {
-                    var item = dirWordCount.ElementAt(index);
-                    string itemKey = item.Key;
-                    int itemValue = item.Value;
-                    Console.WriteLine("Sorted values: " + itemKey + "," + itemValue);
-
-                    //create a sorted file & then write to it
-                    using (StreamWriter sw = File.CreateText(sortedFileName))
-                    {
-                        foreach (var entry in dirWordCount)
-                        {
-                            if ((entry.Key) != null)
-                            {
-                                if (entry.Value > 1)
-                                {
-                                    sw.WriteLine("{0}{1}{2}", entry.Key, ",", entry.Value);
-                                }
-                                else
-                                {
-                                    sw.WriteLine("{0}", entry.Key);
-                                }
-                            }
-                        }
-                        sw.Close();
-
-                    }
-
-
-                }
-
-
-                //storing all files with respect to directories
-                //Check for already existing files
-                dictionaryfile.Add(newfile, destPath);
-                   
-
-
-                //writing all sorted files 
-                writeToHistoryFile(newfile,"ListAllFiles");
-
-            }
-            MessageBox.Show("Sorting of files completed!!");
-            //wrtiting  all Directories
-            writeToHistoryFile(destPath,"ListDirectory");
-        }
-
-        private void writeToAllHistoryFile()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void writeToHistoryFile(String files, string flag)
-        {
-            StreamWriter writer = null;
-            Console.WriteLine("writing files to history file");
-
-
-            if (flag.Equals("ListDirectory"))
-            {
-                try
-                {
-                    string configfilePath = ConfigurationManager.AppSettings["AllDirPath"];
-                    using (writer = File.AppendText(@configfilePath))
-                    {
-                        writer.WriteLine(files);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exception: " + e.Message);
-                }
-                finally
-                {
-                    writer.Close();
-                }
-            }
-            else if (flag.Equals("ListAllFiles")) {
-
-                try
-                {
-                    string configfilePath = ConfigurationManager.AppSettings["AllFilePath"];
-                    using (writer = File.AppendText(@configfilePath))
-                    {
-                        writer.WriteLine(files);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exception: " + e.Message);
-                }
-                finally
-                {
-                    writer.Close();
-                }
-            }
-
-
-        }
-
+        
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
@@ -333,15 +169,7 @@ namespace WindowsFormsFileApplication
             {
                 textBox1.Visible = true;
                 string  directory = file.SelectedPath;
-                string sortedfile = string.Empty;
-                Console.WriteLine("Retreiving all sorted file for given directory");
-                foreach (KeyValuePair<string, string> keyValue in dictionaryfile)
-                {
-                    Console.WriteLine("{0} -> {1}", keyValue.Key, keyValue.Value);
-                    if (keyValue.Value.Equals(directory)) {
-                        sortedfile = sortedfile+Environment.NewLine + keyValue.Key;
-                    }
-                }
+                string sortedfile = fileutils.getAllDirectories(directory);
                 textBox1.Text = @sortedfile;
             }
         }
@@ -353,6 +181,27 @@ namespace WindowsFormsFileApplication
             button3.Visible = false;
             btnSubmit.Visible=false;
 
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog file = new FolderBrowserDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                calcRootPath = file.SelectedPath;
+                MessageBox.Show(calcRootPath);
+                button6.Visible = true;
+
+            }
+        }
+    
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FileCalc lfilecalc = new FileCalc();
+            lfilecalc.calculate(calcRootPath);
+            MessageBox.Show("Completed!!");
         }
     }
 }
